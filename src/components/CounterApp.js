d0b1b22c3c1cbe674178030081a2a93aed932510
Andrew Hale <andyhalebmx@hotmail.com>
2019-08-16 17:15:22 +0100

    odd or even in seperate component

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from '../assets/img/the_count_14aug.svg'
 import Count from './Count';
+import OddOrEven from './OddOrEven';
 import { Button, Title, Reset } from '../style/components/Main';
 
 const CounterApp = () => {
@@ -20,45 +21,21 @@ const CounterApp = () => {
         setCount(0);
     }
 
-
-    const OddOrEven = ({ count }) => {
-        switch(count) {
-        case (count === 0):
-            return <div>Zero</div>;
-        case (count % 2 === 0):
-            return <div>Even</div>;
-        default:
-            return <div>Odd</div>;
-        }
-    }
-
-    // const oddOrEven = (count) => {
-    //     if (count === 0) {
-    //         return ("Zero");
-    //     } else {
-    //         if (count % 2 === 0) {
-    //             console.log("even");
-    //             return ("Even");
-    //         } else {
-    //             console.log("odd");
-    //             return ("Odd");
-    //         }
-    //     }
-    // }
-
-    let resetClass = "";
-    resetClass += (count === 0) ? "red-bg":"white-bg";
-
     return (
         <div className="wrapper">
             <img src={logo} width="400" height="400" alt="the count"></img>
             <Title>The Count</Title>
             <Count initialCount={count}/>
-            <p className="oddOrEven">This number is {OddOrEven(count)}</p>
+            <div className="oddOrEven">
+                This number is
+                <OddOrEven oddeven={count} />
+            </div>
             <div>
                 <Button className='button' onClick={handleDecrement}>-</Button>
                 <Button className='button' onClick={handleIncrement}>+</Button>
-                <Reset className={resetClass} onClick={handleReset}>Reset</Reset>
+                <Reset className={count <= 0 ? 'red-bg' : 'white-bg'} onClick={handleReset}>
+                    Reset
+                </Reset>
             </div>
         </div>
     )
--- src/components/OddOrEven.js
+++ src/components/OddOrEven.js
@@ -1,30 +1,13 @@
 import React from 'react';
 
-
-const OddOrEven = ({ count }) => {
-    switch(count) {
-      case (count === 0):
-        return <span>"Zero"</span>;
-      case (count % 2 === 0):
-        return <span>"Even"</span>;
-      default:
-        return <span>"Odd"</span>;
+const OddOrEven = ({ oddeven }) => {
+    if (oddeven === 0) {
+        return (<div>&nbsp;Zero</div>);
+    } else if (oddeven % 2 === 0) {
+        return (<div>&nbsp;Even</div>);
+    } else {
+        return (<div>&nbsp;Odd</div>);
     }
 }
-
-// const OddOrEven = ({count}) => {
-//     // console.log("working");
-//     if (count === 0) {
-//         return ("Zero");
-//     } else {
-//         if (count % 2 === 0) {
-//             // console.log("even");
-//             return ("Even");
-//         } else {
-//             // console.log("odd");
-//             return ("Odd");
-//         }
-//     }
-// }
 
 export default OddOrEven;
